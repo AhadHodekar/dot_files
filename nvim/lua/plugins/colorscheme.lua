7d3e45efcd56321e94fefc1b232c7879d6b25995
Ahad Hodekar <ahadparwezhodekar@gmail.com>
2025-01-02 07:55:31 +0000

    nvim config

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    -- "catppuccin/nvim",
+    "craftzdog/solarized-osaka.nvim",
+    -- "shaunsingh/nord.nvim",
+    -- "bluz71/vim-nightfly-colors",
+    -- "arzg/vim-colors-xcode",
+    lazy = true,
+    priority = 1000,
+    -- opts = function()
+    --   return {
+    --     transparent = true,
+    --   }
+    -- end,
+  },
+  -- {},
+}
